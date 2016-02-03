2e3d40f82a3a85887ee066598e945c302ebf0325
deads2k <deads@redhat.com>
2016-02-03 10:08:53 -0500

    squash to registration changes

--- pkg/authorization/api/install/install.go
+++ pkg/authorization/api/install/install.go
@@ -110,7 +110,7 @@ func addVersionsToScheme(externalVersions ...unversioned.GroupVersion) {
 }
 
 func newRESTMapper(externalVersions []unversioned.GroupVersion) meta.RESTMapper {
-	rootScoped := sets.NewString("SubjectAccessReview", "ResourceAccessReview", "ClusterRole", "ClusterRoleBinding", "ClusterPolicy", "ClusterPolicyBinding")
+	rootScoped := sets.NewString("ClusterRole", "ClusterRoleBinding", "ClusterPolicy", "ClusterPolicyBinding")
 	ignoredKinds := sets.NewString()
 	return kapi.NewDefaultRESTMapper(externalVersions, interfacesFor, importPrefix, ignoredKinds, rootScoped)
 }
